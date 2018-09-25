fe975ff3fa624b43b17042cc63151775c2680ab0
turkeyaa <18668089860@163.com>
2018-09-25 09:35:27 +0800

    基于DNSPageView库的多视图切换

--- TemplateSwiftAPP/TemplateSwiftAPP/Base module/Common/VC/BasePageVC.swift
+++ TemplateSwiftAPP/TemplateSwiftAPP/Base module/Common/VC/BasePageVC.swift
@@ -8,97 +8,38 @@
 
 import Foundation
 import UIKit
+import DNSPageView
 
 /// todo - 待优化
 class BasePageVC: BaseVC {
     
     private var titles = [String]()
     private var controllers = [UIViewController]()
-
-    private var currentIndex: Int = 0
-    
-    lazy var titleIndexView: TitleIndexView = {
-        
-        var pointY = Device_status
-        if !self.isHideNav {
-            pointY += Device_nav
-        }
-        let view = TitleIndexView.init(titles: titles, frame: CGRect.init(x: 0, y: pointY, width: Device_width, height: 50))
-        view.clickItemBlock = { (index: Int) -> Void in
-            self.currentIndex = index
-            self.exChangeTopViewIndex(index: index, type: true)
-        }
-        return view
-    }()
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titles = self.configTitles()
-        controllers = self.configControlles()
+        // 创建DNSPageStyle，设置样式
+        let style = DNSPageStyle()
+        style.isTitleScaleEnabled = true
+        style.titleSelectedColor = .orange
         
-        self.view.addSubview(titleIndexView)
-        titleIndexView.updateUI()
+        // 设置标题内容
+        let titles = configTitles()
         
-        configu()
-        configuGesture()
-    }
-    
-    private func exChangeTopViewIndex(index: Int, type: Bool) -> Void {
+        // 创建每一页对应的controller
+        let childViewControllers: [UIViewController] = configControlles()
         
-        let view = controllers[index].view!
-        self.view.bringSubview(toFront: view)
+        let size = UIScreen.main.bounds.size
         
-        if type {
-            view.frame = CGRect.init(x: Device_height, y: view.frame.origin.y, width: Device_width, height: view.frame.size.height)
-            UIView.animate(withDuration: 0.3) {
-                view.frame = CGRect.init(x: 0, y: view.frame.origin.y, width: Device_width, height: view.frame.size.height)
-            }
-        } else {
-            view.frame = CGRect.init(x: -Device_height, y: view.frame.origin.y, width: Device_width, height: view.frame.size.height)
-            UIView.animate(withDuration: 0.3) {
-                view.frame = CGRect.init(x: 0, y: view.frame.origin.y, width: Device_width, height: view.frame.size.height)
-            }
+        // 创建对应的DNSPageView，并设置它的frame
+        // titleView和contentView会连在一起
+        var paddingX: CGFloat = 64
+        if !self.isHideNav {
+            paddingX += 20
         }
-    }
-    
-    func configu() -> Void {
-        for vc in controllers {
-            self.addChildViewController(vc)
-            vc.view.frame = CGRect.init(x: 0, y: titleIndexView.frame.size.height+titleIndexView.frame.origin.y, width: Device_width, height: self.view.frame.size.height-titleIndexView.frame.size.height-titleIndexView.frame.origin.y)
-            self.view.addSubview(vc.view)
-        }
-        exChangeTopViewIndex(index: currentIndex, type: true)
-    }
-    
-    func configuGesture() -> Void {
-        let swipeLeft = UISwipeGestureRecognizer.init(target: self, action: #selector(swipeLeftEvent))
-        swipeLeft.direction = .left
-        self.view.addGestureRecognizer(swipeLeft)
-        
-        let swipeRight = UISwipeGestureRecognizer.init(target: self, action: #selector(swipeRightEvent))
-        swipeRight.direction = .right
-        self.view.addGestureRecognizer(swipeRight)
-    }
-    
-    @objc func swipeLeftEvent() -> Void {
-        currentIndex = currentIndex + 1
-        if currentIndex > controllers.count - 1 {
-            currentIndex = controllers.count - 1
-            return
-        }
-        exChangeTopViewIndex(index: currentIndex, type: true)
-        self.titleIndexView.currentIndex = currentIndex
-    }
-    
-    @objc func swipeRightEvent() -> Void {
-        currentIndex = currentIndex - 1
-        if currentIndex < 0 {
-            currentIndex = 0
-            return
-        }
-        exChangeTopViewIndex(index: currentIndex, type: false)
-        self.titleIndexView.currentIndex = currentIndex
+        let pageView = DNSPageView(frame: CGRect(x: 0, y: paddingX, width: size.width, height: size.height), style: style, titles: titles, childViewControllers: childViewControllers)
+        view.addSubview(pageView)
     }
     
     func configTitles() -> [String] {
